968bc7e7869528255f7c4f8a1341517866d04c8a
Vidsify <Vidsify@hotmail.com>
2020-11-14 19:09:33 +0000

    More

--- src/config/production.ts
+++ src/config/production.ts
@@ -2,6 +2,7 @@ export const config: any = {
     token: "NTQzMTQxMzU4NDk2MzgzMDQ4.XFx9zg.CIyB_aEeSR8k__MvC6u2ZLc3fwA",
     linkerId: "282602523569815552",
     minecraftVersion: "1.12.2-1.15.2",
-    minecraftIP: "play.palace.network"
+    minecraftIP: "play.palace.network",
+    applyAddress: "https://palnet.us/apply"
 }
 // I want to convert this to a JSON based config 
