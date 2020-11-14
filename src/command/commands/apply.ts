c7ed991c9f423b86ab2906c40f87b882cb6d2e06
Vidsify <Vidsify@hotmail.com>
2020-11-14 19:09:55 +0000

    More

--- src/command/commands/apply.ts
+++ src/command/commands/apply.ts
@@ -1,5 +1,6 @@
 import { ICommand, CommandResponse } from "./command";
 import { DiscordBot } from "../../server";
+import { config } from "../../config/production";
 
 export class Apply implements ICommand {
     commandName: string = "apply";
@@ -7,7 +8,7 @@ export class Apply implements ICommand {
     buildResponse(user: number, message: any, bot: DiscordBot): Promise<CommandResponse> {
         return new Promise<CommandResponse>((resolve, reject) => {
             const reponse: CommandResponse = {
-                response: "We are always offering new oppourtunites to join our team, see what positions are available at https://palnet.us/apply",
+                response: `We are always offering new oppourtunites to join our team, see what positions are available at ${config.applyAddress}.`,
                 channelId: message.channel,
                 mention: true
             }
