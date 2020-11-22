654d2f93a983b5232de4c5320d5a2bd1f253c471
Vidsify <vidsify@hotmail.com>
2020-11-22 18:00:31 +0000

    Added Template Config

--- src/config/template.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    token: "NTQzMTQxMzU4NDk2MzgzMDQ4.Xtq5Jg.LvQSYcw5oLxWoEPHzT4a5thcDFY",
-    linkerId: "282602523569815552",
-    minecraftVersion: "1.12.2-1.15.2"
-};
-
-// I want to convert this to a JSON based config 
